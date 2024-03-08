a0e4738e8d53190aa994333647d7bc59b470de65
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-08 22:57:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
+
+const App = () => {
+  return (
+    <div>
+      <Banner/>
+      <Footer/>
+      <Navbar/>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
