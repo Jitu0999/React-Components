444672036581684949737892993881740011476c
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-08 23:55:19 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      Footer (The website footer is the bottom section, offering essential info (copyright, contact) and navigation (sitemap).It can also include social media links and be visually designed to complement the site.)
+      Footer (The website "footer" is the bottom section, offering essential info (copyright, contact) and navigation (sitemap).It can also include social media links and be visually designed to complement the site.)
     </div>
   )
 }
