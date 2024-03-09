34681aa0f3baa94520c021749812f438b6eae1aa
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-09 23:30:11 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div>
-      Navbar. (A navigation bar (or navbar) is a menu that helps users navigate websites and apps. Found at the top or bottom of a webpage, it contains links to different sections. Essentially, it's a roadmap for users)
+      Navbar. (A navigation bar (or navbar) is a menu that helps users navigate websites and apps. Found at the top or bottom of a webpage, it contains links to different sections. Essentially, it's a roadmap for users to find what they're looking for.)
     </div>
   )
 }
