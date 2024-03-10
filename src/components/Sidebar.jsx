f15a805bc712af4e9deb335a34322200b67fc295
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-10 23:04:49 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 const Sidebar = () => {
   return (
     <div>
-      Sidebar. (The sidebar navigation is the vertical list of links in the left panel of)
+      Sidebar. (The sidebar navigation is the vertical list of links in the left panel of )
     </div>
   )
 }
