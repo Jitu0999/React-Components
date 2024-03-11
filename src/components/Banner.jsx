99df0658a073498ca6de62889b9113cbb633ddd5
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-11 23:11:03 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
     <div>
-      <h3>Banner.</h3> (Banners on websites are like ads or big images that grab your attention.)
+      <h3>Banner.</h3>Banners on websites are like ads or big images that grab your attention.)
     </div>
   )
 }
