aa25c356606315b49d1cce02b521f9841cb97a4e
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-11 23:00:48 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 const Sidebar = () => {
   return (
     <div>
-      <h3>Sidebar.</h3> (The sidebar navigation is the vertical list of links in the left panel of some webpages. Microsite webpages are required to have sidebar navigation, except the homepage and pages using the wide template.)
+      <h3>Sidebar.</h3>
+      <p>The sidebar navigation is the vertical list of links in the left panel of some webpages. Microsite webpages are required to have sidebar navigation, except the homepage and pages using the wide template.</p>
     </div>
   )
 }
